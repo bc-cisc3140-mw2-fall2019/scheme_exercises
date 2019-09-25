b001a9e10fe0797d39ec6b017984b193b723d6c8
Hareem Bokhari <hbokhari98@gmail.com>
2019-09-25 14:54:11 -0400

    QA answered #1

--- 01.scm
+++ 01.scm
@@ -1,20 +1,20 @@
-; What do the following Scheme expressions evaluate to?
+; ; What do the following Scheme expressions evaluate to?
+; ; (a) (* 2 (+ 4 5))
+
+; ; Code here to allow running in editor 
 ; (a) (* 2 (+ 4 5))
+; 18 ; the sum of 4 and 5 multiplied by 2
 
-; Code here to allow running in editor 
-(a) (* 2 (+ 4 5))
-18 ; the sum of 4 and 5 multiplied by 2
+; (b) (= 3 (+ 1 3))
+; #f ; The boolean false
 
-(b) (= 3 (+ 1 3))
-#f ; The boolean false
+; (c) (car ’(elmer fudd daffy duck))
+; elmer 
 
-(c) (car ’(elmer fudd daffy duck))
-elmer 
+; (d) (cdr ’(elmer fudd daffy duck))
+; (fudd daffy duck)
 
-(d) (cdr ’(elmer fudd daffy duck))
-(fudd daffy duck)
+; (e) (and (= 1 2) (= 10 (/ 1 0)))
+; #f 
 
-(e) (and (= 1 2) (= 10 (/ 1 0)))
-#f 
-
- group QA
+;  group QA
