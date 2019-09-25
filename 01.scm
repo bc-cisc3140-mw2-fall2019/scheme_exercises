0eab70065aaef4fba4b5a4a58db15d29789633e3
Hareem Bokhari <hbokhari98@gmail.com>
2019-09-25 14:53:34 -0400

    QA answered #1

--- 01.scm
+++ 01.scm
@@ -2,10 +2,19 @@
 ; (a) (* 2 (+ 4 5))
 
 ; Code here to allow running in editor 
-(* 2 (+ 4 5))
+(a) (* 2 (+ 4 5))
+18 ; the sum of 4 and 5 multiplied by 2
 
-; (b) (= 3 (+ 1 3))
-; (c) (car ’(elmer fudd daffy duck))
-; (d) (cdr ’(elmer fudd daffy duck))
-; (e) (and (= 1 2) (= 10 (/ 1 0)))
+(b) (= 3 (+ 1 3))
+#f ; The boolean false
+
+(c) (car ’(elmer fudd daffy duck))
+elmer 
+
+(d) (cdr ’(elmer fudd daffy duck))
+(fudd daffy duck)
+
+(e) (and (= 1 2) (= 10 (/ 1 0)))
+#f 
+
  group QA
