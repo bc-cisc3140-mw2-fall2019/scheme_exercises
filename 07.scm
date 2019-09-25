4035e572102dda3f6165d4ac12bddc5d2389e22b
Hareem Bokhari <hbokhari98@gmail.com>
2019-09-25 15:13:51 -0400

    explain

--- 07.scm
+++ 07.scm
@@ -1,8 +1,9 @@
 07.scm
 
-(define (sum s)
-	(sum-helper s 0))
-(define (sum-helper s sofar)
+(define (sum s) ; defined the function sum that takes variable s
+	(sum-helper s 0)) ; pass the variable s and 0 to sum-helper
+(define (sum-helper s sofar) 
 	(if (null? s)
 		sofar
-		(sum-helper (cdr s) (+ (car s) sofar))))
+		(sum-helper (cdr s) (+ (car s) sofar)))) 
+		; recursively adding to sum while checking if null.
