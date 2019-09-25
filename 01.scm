ec7c776f2bc6c2137228db256e4982cfa7ecb076
katychuang <katychuang@gmail.com>
2019-09-25 13:57:05 -0400

    start problem 1

---
+++ 01.scm
@@ -0,0 +1,10 @@
+; What do the following Scheme expressions evaluate to?
+; (a) (* 2 (+ 4 5))
+
+; Code here to allow running in editor 
+(* 2 (+ 4 5))
+
+; (b) (= 3 (+ 1 3))
+; (c) (car ’(elmer fudd daffy duck))
+; (d) (cdr ’(elmer fudd daffy duck))
+; (e) (and (= 1 2) (= 10 (/ 1 0)))
